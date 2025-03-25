76a112398e926b5b141709bc5869794e5caa911c
Filipp Voronov <fivoronov@gmail.com>
2025-03-25 18:55:15 +0300

    New user created

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ users = [
     {"nickname": "Alex", "age": 50, "rating": 4.3},
     {"nickname": "Yulia", "age": 26, "rating": 3.2},
     {"nickname": "Sveta", "age": 32, "rating": 5.0},
+    {"nickname": "Katya", "age": 23, "rating": 4.8},
     {"nickname": "Sasha", "age": 84, "rating": 1.2}
 ]
 
