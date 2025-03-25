8355c2c64cb6d066b0e59fb0706f1d8db7c206b4
Filipp Voronov <fivoronov@gmail.com>
2025-03-25 19:42:30 +0300

    Age of Yulia changed from 26 to 25

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Старт программы")
 
 users = [
     {"nickname": "Alex", "age": 50, "rating": 4.3},
-    {"nickname": "Yulia", "age": 26, "rating": 3.2},
+    {"nickname": "Yulia", "age": 25, "rating": 3.2},
     {"nickname": "Sveta", "age": 32, "rating": 5.0},
     {"nickname": "Katya", "age": 23, "rating": 4.8},
     {"nickname": "Sasha", "age": 84, "rating": 1.2}
